c684fa0d3fbb6601c886c8673f4bbaf0a72abf75
Milind Ninu Phirake <milind.phirake@anchanto.com>
2016-02-19 11:40:19 +0530

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,16 @@
 class Order < ActiveRecord::Base
+	belongs_to :address
+	
 	has_many :line_items
+	
+	belongs_to :customer 
+	
 	has_many :products
 
-	belongs_to :customer 
-	belongs_to :address
 
-	accepts_nested_attributes_for :customer , :address , :line_items
-	# accepts_nested_attributes_for :address
-	# accepts_nested_attributes_for :line_items
-
-	# def to_json
-	# 	json = "{"
- #  		json += "id:'#{self.id}'"
- #  		json += ",fname'#{self.fname}'"
- #  		json += "}"
-	# end
+	accepts_nested_attributes_for :customer 
+	
+	#Added New Text
+	
+	accepts_nested_attributes_for :address , :line_items
 end
